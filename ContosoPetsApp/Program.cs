702a3dee6da45a9b769e5107aeb267ad084241c8
Jared Naschenweng <jared@naschenweng.info>
2023-05-23 11:49:34 +0200

    added new code-along prj

---
+++ ContosoPetsApp/Program.cs
@@ -0,0 +1,122 @@
+﻿// ** Contoso Pets Console App
+// todo - requirements
+// 1. store application data in a multidiemensional string array named ourAnimals
+// 2. the ourAnimals array includes the following 'pet characteristics' for each animal:
+//    - pet ID #
+//    - pet species
+//    - pet age (in years)
+//    - pet description - physical characteristics/condition
+//    - pet description - personality/temperament
+//    - pet nickname
+
+// todo - steps
+// 1. implement a sample dataset that represents dogs & cats
+// 2. display menu options to access the main features of the app
+// 3. the main features enable the following tasks:
+//    - list pet information for all animals in the ourAnimals array
+//    - add a new animal to the ourAnimals array, which includes the following information:
+//      - pet species (dog or cat) must be entered when a new animal is added to to the ourAnimals array
+//      - pet id must be programmatically generated when a new animal is added to the ourAnimals array
+//      - some physical characteristics for a pet may be unknown at the time the pet is added to the ourAnimals array
+//      - an animal's nickname / personality may be unknown at the time the pet is added to the ourAnimals array
+//    - ensure animal ages and physical characteristics are complete and accurate
+//    - ensure animal nicknames and personality traits are complete and accurate
+//    - edit an animal's age
+//    - edit an animal's physical characteristics (personality, temperament, etc.)
+//    - display all cats that meet the user specified criteria
+//    - display all dogs that meet the user specified criteria
+
+// the ourAnimals array will store the following: 
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+// variables that support data entry
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+
+// array used to store runtime data, there is no persisted data
+string[,] ourAnimals = new string[maxPets, 6];
+
+// TODO: Convert the if-elseif-else construct to a switch statement
+
+// create some initial ourAnimals array entries
+for (int i = 0; i < maxPets; i++)
+{
+    switch (i)
+    {
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+            break;
+        case 2:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "loki";
+            break;
+        case 3:
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "Puss";
+            break;
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+    }
+
+
+
+    ourAnimals[i, 0] = "ID #: " + animalID;
+    ourAnimals[i, 1] = "Species: " + animalSpecies;
+    ourAnimals[i, 2] = "Age: " + animalAge;
+    ourAnimals[i, 3] = "Nickname: " + animalNickname;
+    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+}
+
+// display the top-level menu options
+
+Console.Clear();
+
+Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+Console.WriteLine(" 1. List all of our current pet information");
+Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+Console.WriteLine(" 5. Edit an animal’s age");
+Console.WriteLine(" 6. Edit an animal’s personality description");
+Console.WriteLine(" 7. Display all cats with a specified characteristic");
+Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+Console.WriteLine();
+Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+readResult = Console.ReadLine();
+if (readResult != null)
+{
+    menuSelection = readResult.ToLower();
+}
+
+Console.WriteLine($"You selected menu option {menuSelection}.");
+Console.WriteLine("Press the Enter key to continue");
+
+// pause code execution
+readResult = Console.ReadLine();
