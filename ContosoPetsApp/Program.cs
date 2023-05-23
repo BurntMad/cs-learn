ff742113f8bc8e856f39dfd706de60af912306e5
Jared Naschenweng <jared@naschenweng.info>
2023-05-23 13:58:49 +0200

    added case 2 loop structure

--- ContosoPetsApp/Program.cs
+++ ContosoPetsApp/Program.cs
@@ -125,13 +125,41 @@ do
     {
         case "1":
             // list all current pet information
-            Console.WriteLine("This app is coming soon - please check back to see progress");
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 6; j++)
+                    {
+                        Console.WriteLine(ourAnimals[i, j]);
+                    }
+                }
+            }
             Console.WriteLine("Press the Enter key to continue");
             readResult = Console.ReadLine();
             break;
         case "2":
-            // add a new friend to the ourAnimals array
-            Console.WriteLine("This app is coming soon - please check back to see progress");
+            // Add a new animal friend to the ourAnimals array
+            string anotherPet = "y";
+            int petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+
+            }
+
+            if (petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
+            }
+
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
             Console.WriteLine("Press the Enter key to continue");
             readResult = Console.ReadLine();
             break;
